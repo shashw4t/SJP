23e0d60023e74522e5dc775a9f3d431d6cd030c2
Shashwat Singh <41077597+shashw4t@users.noreply.github.com>
2024-08-09 12:10:25 -0400

    chore: Update site metadata in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,46 +1,44 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
 
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
-module.exports = {
-  siteMetadata: {
-    title: `My Custom Website`,
-    description: `Welcome to my custom Gatsby website.`,
-    author: `@myhandle`,
-    siteUrl: `https://shashw4t.netlify.app/`,
-  },
-  plugins: [
-    // Your existing plugins
-  ],
-}
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+  /**
+   * Configure your Gatsby site with this file.
+   *
+   * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+   */
+
+  /**
+   * @type {import('gatsby').GatsbyConfig}
+   */
+  module.exports = {
+    siteMetadata: {
+      title: `My Custom Website`,
+      description: `Welcome to my custom Gatsby website.`,
+      author: `@myhandle`,
+      siteUrl: `https://shashw4t.netlify.app/`,
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+    plugins: [
+      `gatsby-plugin-image`,
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: `images`,
+          path: `${__dirname}/src/images`,
+        },
       },
-    },
-  ],
-}
+      `gatsby-transformer-sharp`,
+      `gatsby-plugin-sharp`,
+      {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+          name: `gatsby-starter-default`,
+          short_name: `starter`,
+          start_url: `/`,
+          background_color: `#663399`,
+          // This will impact how browsers show your PWA/website
+          // https://css-tricks.com/meta-theme-color-and-trickery/
+          // theme_color: `#663399`,
+          display: `minimal-ui`,
+          icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        },
+      },
+    ],
+  }
