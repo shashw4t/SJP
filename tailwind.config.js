17a1b7d76f4b1839a662381d9a57221c948489e8
Shashwat Singh <41077597+shashw4t@users.noreply.github.com>
2024-08-20 14:27:25 -0400

    Update colors in globals.css to match new design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,11 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
-    extend: {
-      colors: {
-        deepPurple: {
-          100: '#212531',
-          200: '#322f42',
-          300: '#4b3a70',
-          400: '#b7a2c9',
-          500: '#c5c3c4',
-        },
-      },
-    },
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
+  darkMode: 'media', // or remove this line entirely
 }
