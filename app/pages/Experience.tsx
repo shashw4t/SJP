332318f5aaa6e3a0fdc7a1f99d467a13d1fade32
Shashwat Singh <41077597+shashw4t@users.noreply.github.com>
2024-08-20 18:57:17 -0400

    Update Experience component with job details and descriptions

--- app/pages/Experience.tsx
+++ app/pages/Experience.tsx
@@ -1,60 +1,87 @@
-"use client";
 import React from "react";
-import Image from "next/image";
 
-
-const NavigationLink = ({ href, title, description }: { href: string, title: string, description: string }) => {
-  const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-    e.preventDefault();
-    const targetId = href.substring(1);
-    const targetElement = document.getElementById(targetId);
-    if (targetElement) {
-      window.scrollTo({
-        top: targetElement.offsetTop,
-        behavior: "smooth",
-      });
-    }
-  };
-
+const Experience = () => {
   return (
-    <a href={href} onClick={handleClick} className="cursor-pointer no-underline text-inherit text-right">
-      <h2 className="mb-3 text-2xl font-semibold">
-        {title}
-        <span className="inline-block transition-transform transform translate-x-0">
-          -&gt;
-        </span>
-      </h2>
-      <p className="m-0 max-w-xs text-sm opacity-50">{description}</p>
-    </a>
+    <section className="experience-section p-6 bg-gray-100">
+      <h2 className="section-title text-center">Experience</h2>
+      <div className="experience-item mb-6">
+        <h3 className="job-title">Software Engineer (SWE)</h3>
+        <p className="company-name">HTD Talent</p>
+        <p className="job-duration">Jul 2024 - Present</p>
+        <ul className="job-list">
+          <li className="job-list-item"><strong>Training:</strong> Delivered two intensive 5-week programs on Java, OOP, SQL, Docker, and MVC.</li>
+          <li className="job-list-item"><strong>Projects:</strong> Developed a versatile reservation system and a comprehensive full-stack website.</li>
+          <li className="job-list-item"><strong>Deployment:</strong> Successfully deployed tailored software solutions to client sites.</li>
+          <li className="job-list-item"><strong>Impact:</strong> Leveraged advanced skills to drive significant improvements in client projects.</li>
+        </ul>
+      </div>
+      <div className="experience-item mb-6">
+        <h3 className="job-title">Software Development Engineer</h3>
+        <p className="company-name">Amazon</p>
+        <p className="job-duration">Aug 2021 - Jul 2022</p>
+        <ul className="job-list">
+          <li className="job-list-item"><strong>Development:</strong> Enhanced Collections Workbench using Java, Python, and Bash scripts.</li>
+          <li className="job-list-item"><strong>Integration:</strong> Connected AWS services with billing and finance teams for seamless data access.</li>
+          <li className="job-list-item"><strong>Maintenance:</strong> Continuously deployed and maintained critical finance automation tools.</li>
+          <li className="job-list-item"><strong>Impact:</strong> Improved efficiency in finance operations and customer data accessibility.</li>
+        </ul>
+      </div>
+      <div className="experience-item mb-6">
+        <h3 className="job-title">Software Engineer</h3>
+        <p className="company-name">Tata Consultancy Services</p>
+        <p className="job-duration">Feb 2021 - Jul 2022</p>
+        <ul className="job-list">
+          <li className="job-list-item"><strong>Consulting:</strong> Provided expert software engineering consultancy for diverse client projects.</li>
+          <li className="job-list-item"><strong>Development:</strong> Designed and implemented solutions using Java and SQL.</li>
+          <li className="job-list-item"><strong>Optimization:</strong> Achieved a 20% reduction in processing time through system enhancements.</li>
+          <li className="job-list-item"><strong>Collaboration:</strong> Improved client satisfaction by 25% with tailored solutions.</li>
+        </ul>
+      </div>
+      <div className="experience-item mb-6">
+        <h3 className="job-title">Web Developer</h3>
+        <p className="company-name">Knowt</p>
+        <p className="job-duration">Jul 2019 - Apr 2020</p>
+        <ul className="job-list">
+          <li className="job-list-item"><strong>Development:</strong> Developed and optimized Knowt’s website and web application using React and Node.js.</li>
+          <li className="job-list-item"><strong>UI/UX Enhancement:</strong> Improved UI design, leading to a 30% increase in user engagement.</li>
+          <li className="job-list-item"><strong>Platform Improvement:</strong> Revamped teacher platform functionality and reduced load times by 25%.</li>
+          <li className="job-list-item"><strong>Collaboration:</strong> Ensured seamless feature integration with the design team.</li>
+        </ul>
+      </div>
+      <div className="experience-item mb-6">
+        <h3 className="job-title">Information Technology Consultant</h3>
+        <p className="company-name">Rutgers Office of Information Technology</p>
+        <p className="job-duration">Mar 2018 - Dec 2018</p>
+        <ul className="job-list">
+          <li className="job-list-item"><strong>Technical Support:</strong> Provided comprehensive support, resolving 95% of issues.</li>
+          <li className="job-list-item"><strong>Troubleshooting:</strong> Diagnosed and fixed hardware/software problems, improving lab efficiency by 20%.</li>
+          <li className="job-list-item"><strong>Lab Management:</strong> Managed computer labs with 100% equipment uptime and user satisfaction.</li>
+          <li className="job-list-item"><strong>Maintenance:</strong> Extended equipment lifespan by 15% through regular maintenance.</li>
+        </ul>
+      </div>
+      <div className="experience-item mb-6">
+        <h3 className="job-title">Application Developer</h3>
+        <p className="company-name">Innovyt</p>
+        <p className="job-duration">May 2018 - Aug 2018</p>
+        <ul className="job-list">
+          <li className="job-list-item"><strong>Blockchain Research:</strong> Investigated blockchain technology for innovative solutions.</li>
+          <li className="job-list-item"><strong>Backend Development:</strong> Built and optimized OilChain’s backend using Node.js and WebSocket.</li>
+          <li className="job-list-item"><strong>API Implementation:</strong> Developed a robust API with Express, ensuring seamless data interaction.</li>
+          <li className="job-list-item"><strong>Testing:</strong> Implemented unit tests with Jest, enhancing application reliability.</li>
+        </ul>
+      </div>
+      <div className="experience-item mb-6">
+        <h3 className="job-title">Lab Researcher</h3>
+        <p className="company-name">Evolutionary Ecology Lab</p>
+        <p className="job-duration">May 2017 - Sep 2017</p>
+        <ul className="job-list">
+          <li className="job-list-item"><strong>Field Research:</strong> Collected samples for studies on plant species and analyzed data.</li>
+          <li className="job-list-item"><strong>Propagation Techniques:</strong> Assisted with plant propagation and applied growth hormones.</li>
+          <li className="job-list-item"><strong>Experimental Contributions:</strong> Supported key research objectives in plant growth studies.</li>
+        </ul>
+      </div>
+    </section>
   );
 };
 
-export default function LandingPage() {
-  return (
-    <div className="bg-night text-beaver min-h-screen flex flex-col items-center justify-center text-right">
-      <Image src="/app/background.jpg" alt="Landing" width={500} height={300} />
-      <nav className="mb-32 grid text-center w-full max-w-5xl grid-cols-1 gap-8 mt-8 text-right">
-        <NavigationLink
-          href="#summary"
-          title="Summary"
-          description="View my professional experience and education."
-        />
-        <NavigationLink
-          href="#portfolio"
-          title="Portfolio"
-          description="Explore my projects and accomplishments."
-        />
-        <NavigationLink
-          href="#education"
-          title="Education"
-          description="Learn about my academic background."
-        />
-        <NavigationLink
-          href="#experience"
-          title="Experience"
-          description="Discover my work experience."
-        />
-      </nav>
-    </div>
-  );
-}
+export default Experience;
